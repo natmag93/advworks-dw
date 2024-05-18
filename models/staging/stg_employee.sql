82e8f181e9c90c9b62ea08a4fdb8024557eb51e0
Zita Martins <zita.sa.martins@gmail.com>
2024-05-18 15:45:23 +0000

    Change in stg_employee e staging snapshot

--- models/staging/stg_employee.sql
+++ models/staging/stg_employee.sql
@@ -1,16 +1,15 @@
 select
     e.businessentityid as employee_id,
-    e.jobtitle as job_title,
+	p.firstname as first_name,
+	coalesce (nullif(p.middlename,''), '') as middle_name,
+    p.lastname as last_name,
     e.birthdate as birth_date,
     e.gender,
-    e.organizationnode as organization_node,
-    e.rowguid as employee_row_guid,
-    e.modifieddate as employee_last_update,
-    p.firstname as first_name,
-    p.middlename as middle_name,
-    p.lastname as last_name,
-    p.rowguid as person_row_guid,
-    p.modifieddate as person_last_update,
-    GREATEST(e.modifieddate, p.modifieddate) as last_update
+	ph.phonenumber as phone_number,
+	ea.emailaddress as email,
+	e.jobtitle as job_title,
+    e.organizationnode as organization_node
 from {{ source("humanresources", "employee") }} e
     left join {{ source("person", "person") }} p on e.businessentityid = p.businessentityid
+    left join {{ source("person", "personphone") }} ph on ph.businessentityid = p.businessentityid
+    left join {{ source("person", "emailaddress") }} ea on ea.businessentityid = p.businessentityid
