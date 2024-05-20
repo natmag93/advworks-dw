d4ebff83a1da587414169505703795081108e3ab
Francisco Leite <franciscoasleite@gmail.com>
2024-05-20 10:07:26 +0000

    round product_total  to 0,01 fix duplicated in fct_purchasing

--- models/staging/stg_purchasing_detail.sql
+++ models/staging/stg_purchasing_detail.sql
@@ -5,6 +5,6 @@ od.productid as product_id,
 od.orderqty:: numeric(8,2),
 od.receivedqty as qty_received,
 ROUND(od.unitprice,2) as unit_price,
-nullif(od.orderqty*od.unitprice,(0.00)) as product_total
+ROUND(nullif(od.orderqty*od.unitprice,(0.00)),2) as product_total
 from {{ source("purchasing", "purchaseorderdetail") }} od
 order by purchase_id
